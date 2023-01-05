17e7a79e002c6c0e7653cb56f2e0fcb422c46b37
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2023-01-05 08:53:36 +0300

    Add changes

--- schema.sql
+++ schema.sql
@@ -22,7 +22,11 @@ CREATE TABLE species (id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL, 
 
 /* Modify animals table (ID already set to auto_increment while creating the table on line no.5) */
 ALTER TABLE animals DROP COLUMN species;
+
 ALTER TABLE animals ADD COLUMN species_id int;
+
 ALTER TABLE animals ADD CONSTRAINT fk_name FOREIGN KEY(species_id) REFERENCES species(id);
+
 ALTER TABLE animals ADD owner_id int;
+
 ALTER TABLE animals ADD CONSTRAINT fk_owners FOREIGN KEY(owner_id) REFERENCES owners(id);
